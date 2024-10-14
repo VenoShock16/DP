5d1d65a6f26b410045650ac644e15eb8d6c0d60d
VenoShock16 <josedpulido@gmail.com>
2024-10-14 18:57:40 +0200

    WareHouse a medias

---
+++ WareHouse.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+/**
+ * Write a description of class WareHouse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WareHouse
+{
+    // instance variables - replace the example below with your own
+    private Location location;
+    private List<Order> orders;
+    /**
+     * Constructor for objects of class WareHouse
+     */
+    public WareHouse()
+    {
+        // initialise instance variables
+      //  x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+       // return x + y;
+       return 1;
+    }
+}
